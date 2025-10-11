77d5fdc7e4d4d5bd03ff43aa62bf31c51a1fb560
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2025-10-11 17:30:20 +0800

    style  fix

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,13 +1,13 @@
 <template>
   <main class="dash__main">
     <section class="col col--left">
+      <div class="panel h360">
+        <span class="title-img title-img--industry" aria-hidden="true"></span>
+
+      </div>
       <div class="panel h180">
         <span class="title-img title-img--org-total" aria-hidden="true"></span>
         <OrgTotal />
-      </div>
-      <div class="panel h360">
-        <span class="title-img title-img--industry" aria-hidden="true"></span>
-        <OrgIndustry />
       </div>
       <div class="panel h300">
         <OrgNew />
@@ -76,8 +76,7 @@ function goToHome() { router.push({ path: '/home', query: { kw: keyword.value ||
 }
 
 .col--left {
-  /* 左列：第一行再减少25px (239px->214px)，最后一行再增加50px (414px->464px) */
-  grid-template-rows: 214px 1fr 464px;
+  grid-template-rows: 1fr 244px 464px;
 }
 
 .col--right {
@@ -112,7 +111,12 @@ function goToHome() { router.push({ path: '/home', query: { kw: keyword.value ||
 /* 左列：工会组织总数（公会组织总数） 151x35 */
 .title-img--org-total { width: 151px; height: 35px; background-image: -webkit-image-set(url('../images/home-title/公会组织总数/编组 21备份.png') 1x, url('../images/home-title/公会组织总数/编组 21备份@2x.png') 2x); background-image: image-set(url('../images/home-title/公会组织总数/编组 21备份.png') 1x, url('../images/home-title/公会组织总数/编组 21备份@2x.png') 2x); }
 /* 左列：工会行业分布（公会行页分布） 151x35 */
-.title-img--industry { width: 151px; height: 35px; background-image: -webkit-image-set(url('../images/home-title/公会行页分布/编组 17.png') 1x, url('../images/home-title/公会行页分布/编组 17@2x.png') 2x); background-image: image-set(url('../images/home-title/公会行页分布/编组 17.png') 1x, url('../images/home-title/公会行页分布/编组 17@2x.png') 2x); }
+.title-img--industry {
+  width: 110px;
+  height: 35px;
+  background-image: -webkit-image-set(url('../images/org-v2/2/编组 21.png') 1x, url('../images/org-v2/2/编组 21@2x.png') 2x);
+  background-image: image-set(url('../images/org-v2/2/编组 21.png') 1x, url('../images/org-v2/2/编组 21@2x.png') 2x);
+}
 /* 左列：新增工会（新增公会） 110x35 */
 .title-img--org-new { width: 110px; height: 35px; background-image: -webkit-image-set(url('../images/home-title/新增公会/编组 18.png') 1x, url('../images/home-title/新增公会/编组 18@2x.png') 2x); background-image: image-set(url('../images/home-title/新增公会/编组 18.png') 1x, url('../images/home-title/新增公会/编组 18@2x.png') 2x); }
 /* 右列：工会会员总数（公会会员总数） 151x35 */
